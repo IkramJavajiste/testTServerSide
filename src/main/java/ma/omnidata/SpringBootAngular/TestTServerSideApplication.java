6520f1f16b9199785fc81c1d068180925a7f5e02
Ikram Naffah <inaffah@omnidata.local>
2018-07-31 11:31:01 +0100

    firstCommitServerSide

---
+++ src/main/java/ma/omnidata/SpringBootAngular/TestTServerSideApplication.java
@@ -0,0 +1,12 @@
+package ma.omnidata.SpringBootAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTServerSideApplication.class, args);
+	}
+}
